1d50cd0d3ccc07061f38dcd52fa7547cfd4b9c8c
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-06-12 12:40:50 -0400

    edit is working

--- smurfs/src/components/smurfs.js
+++ smurfs/src/components/smurfs.js
@@ -23,16 +23,68 @@ const Smurfs = (props) => {
       });
   };
 
+  const editSmurfButton = (e) => {
+    e.preventDefault();
+    setEditToggle(true);
+  };
+  const smurfEditChange = (e) => {
+    e.preventDefault();
+    setEditSmurf({
+      ...editSmurf,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e) => {
+    axios
+      .put(`http://localhost:3333/smurfs/${props.smurf.id}`, editSmurf)
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setEditToggle(false);
+        props.setRefresh(!props.refresh);
+      });
+  };
+  const cancelEdit = (e) => {
+    setEditToggle(false);
+  };
   return (
     <section>
       {editToggle ? (
-        <></>
+        <>
+          <form onSubmit={onSubmit}>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={editSmurf.name}
+              onChange={smurfEditChange}
+            />
+            <label>Height:</label>
+            <input
+              type="text"
+              name="height"
+              value={editSmurf.height}
+              onChange={smurfEditChange}
+            />
+            <label>Age:</label>
+            <input
+              type="text"
+              name="age"
+              value={editSmurf.age}
+              onChange={smurfEditChange}
+            />
+            <button type="submit">Submit</button>
+            <button onClick={cancelEdit}>Cancel Edit</button>
+          </form>
+        </>
       ) : (
         <>
           <h3>{props.smurf.name}</h3>
           <p>height: {props.smurf.height}</p>
           <p>age:{props.smurf.age}</p>
-          <button>Edit</button>
+          <button onClick={editSmurfButton}>Edit</button>
           <button onClick={deleteSmurf}>Delete</button>
         </>
       )}
